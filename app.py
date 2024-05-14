4de516476631fa949ce2698ef62b07a234f9dbca
kesler20 <uchekesla@gmail.com>
2024-05-14 16:25:50 +0100

    feat: added jinja templates and flask😕

--- app.py
+++ app.py
@@ -1,35 +1,28 @@
-from flask import redirect, url_for, render_template
+from flask import render_template
 from flask import Flask
 
-app = Flask(
-    __name__, 
-    template_folder='templates',
-    static_folder='static'
-)
+app = Flask(__name__, template_folder="templates", static_folder="static")
 
-@app.route('/')
+
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/<page>/', methods=['POST','GET'])
-def show(page):
-    if page == 'login':
-        redirect(url_for('logins'))
 
-    elif page == 'account':
-        redirect(url_for('accounts'))
+@app.route("/<route>/", methods=["POST", "GET"])
+def show(route):
+    page = {
+        "home": "index",
+        "about": "about",
+        "contact": "contact",
+        "services": "services",
+        "portfolio": "portfolio",
+        "blog": "blog",
+        "blog-single": "blog-single",
+        "elements": "elements",
+    }.get(route, "404")
+    return render_template(f"{page}.html")
 
-    elif page == 'transactions':
-        redirect(url_for('transactions'))
-    
-    elif page == 'mine':
-        redirect(url_for('miners'))
 
-    elif page == 'blockchain':
-        redirect(url_for('blockchains'))
-    else:
-        pass
-    return render_template(f'{page}.html')
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True, port=5500)
