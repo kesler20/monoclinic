cd8a41b0b4886d7f1e3164380fc1bdabac39a901
kesler20 <uchekesla@gmail.com>
2024-05-22 22:04:55 +0100

    feat: completed the first version of the website😕

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
-from fastapi.responses import HTMLResponse
+from fastapi.responses import FileResponse, HTMLResponse
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
@@ -23,6 +23,18 @@ def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 
+@app.post("/")
+async def handle_form(request: Request, email: str = Form(...)):
+    with open("emails.txt", "a") as f:
+        f.write(email + "\n")
+
+    return templates.TemplateResponse(f"thankyou.html", {"request": request})
+
+
+@app.get("/api/emails", response_class=FileResponse)
+def get_email_responses():
+    return FileResponse(os.path.join(dir_path, "emails.txt"))
+
 @app.api_route("/{route}/", methods=["POST", "GET"], response_class=HTMLResponse)
 def show(request: Request, route: str):
     page = {
@@ -30,5 +42,7 @@ def show(request: Request, route: str):
         "product_overview": "product_overview",
         "case_studies": "case_studies",
         "contact": "contact",
+        "thankyou": "thankyou",
     }.get(route, "404")
+
     return templates.TemplateResponse(f"{page}.html", {"request": request})
